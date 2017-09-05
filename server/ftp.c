928d4c46f9039f17b4f364cf02e0b8b81db71f4c
woyouwukuaiqian <1418957094@qq.com>
2017-09-05 19:05:22 +0800

    child new_fd

--- server/ftp.c
+++ server/ftp.c
@@ -12,7 +12,7 @@ int main()
 	int sfd=socket(AF_INET,SOCK_STREAM,0);
 	if(sfd==-1)
 	{	perror("socket");exit(-1); }
-	int pro_num=1;
+	int pro_num=2;
 	pdata p=(pdata)calloc(pro_num,sizeof(data_t));
 	make_child(p,pro_num);
 
@@ -28,11 +28,62 @@ int main()
 	if(listen(sfd,10)==-1)
 	{	perror("listen");close(sfd);exit(-1); }
 
-	int new_fd=accept(sfd,NULL,NULL);
-	printf("new_fd=%d\n",new_fd);
-	char c[10]={0};
-	recv(new_fd,c,10,0);
-	printf("c=%s\n",c);
+	int epfd=epoll_create(1);
+	if(-1==epfd)
+	{	perror("epoll_create"); exit(-1) ; }
+	
+	struct epoll_event sock;
+	memset(&sock,0,sizeof(sock));
+	sock.events=EPOLLIN;
+	sock.data.fd=sfd;
+	epoll_ctl(epfd,EPOLL_CTL_ADD,sfd,&sock);
+
+	struct epoll_event childEvent;
+	for(int i=0;i<pro_num;++i)
+	{
+		memset(&childEvent,0,sizeof(childEvent));
+		childEvent.events=EPOLLIN;
+		childEvent.data.fd=p[i].sfd;
+		epoll_ctl(epfd,EPOLL_CTL_ADD,p[i].sfd,&childEvent);
+	}
+
+	struct epoll_event* evs=(struct epoll_event*)calloc(pro_num+1,sizeof(epoll_event));
+	int flag;
+	while(1)
+	{
+		ret=epoll_wait(epfd,evs,pro_num+1,-1);
+		for(int i=0;i<ret;++i)
+		{
+			if(evs[i].data.fd==sfd)
+			{
+				int new_fd=accept(sfd,NULL,NULL);
+				printf("ftp:new_fd=%d\n",new_fd);
+				if(new_fd==-1)
+				{	perror("accept");exit(-1); }
+				for(int j=0;j<pro_num;++j)
+				{
+					if(p[j].busy!=1)
+					{
+						send_fd(p[j].sfd,new_fd);
+						close(new_fd);
+						p[j].busy=1;
+						break;
+					}
+				}
+			}
+			for(int j=0;j<pro_num;++j)
+			{
+				if(evs[i].data.fd==p[j].sfd)
+				{
+					read(p[j].sfd,&flag,sizeof(flag));
+					printf("child is not busy\n");
+					p[j].busy=0;
+					break;
+				}
+			}
+		}
+	}
+
 	wait(NULL);
 	return 0;
 
